269980053f3b8a72814d40060a2eec80a772ea3d
lignah <156158159+lignah@users.noreply.github.com>
2024-07-09 20:24:21 +0900

    update TDZ

--- const hoisting window.object.js
+++ const hoisting window.object.js
@@ -51,6 +51,18 @@ var beforehoisting= function() {
 
 
 
+// TDZ, temporal dead zone, let & const
+/* variables declared with let and const enter the "Temporal Dead Zone" (TDZ)
+ * their declarations are hoisted, but initialization occurs at original code location
+ * therefore, accessing these variables before initialization results in referenceError
+*/
+let tdz;           // declaration is hoisted but now its in TDZ
+console.log(tdz);  // referenceError
+tdz = 3;           // exit from TDZ
+
+
+
+
 // in browser(v8), not nodejs
 let x= 1  // not eq window.x
 var y= 2
